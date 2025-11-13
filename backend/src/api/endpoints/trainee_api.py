bb5e5d1ba6a97802cfefdbc2a4193429ac67295c
Arina <ari.kash117@gmail.com>
2025-11-13 20:57:53 +0300

    Fixed api get(list trainees) and post(create trainee)

--- backend/src/api/endpoints/trainee_api.py
+++ backend/src/api/endpoints/trainee_api.py
@@ -1,18 +1,41 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, Query, HTTPException
+from sqlalchemy.orm import Session
+from typing import List, Optional
 
-trainee_router = APIRouter()
+from database.db import get_db
+from schemas.trainee import (
+    TraineesResponse,
+    TraineeResponse,
+    CreateTrainee,
+)
+from crud.trainee import (
+    get_list_trainees,
+    create_trainee,
+)
 
-# POST
-@trainee_router.post('/trainies/{trainee_id}')
-async def post_trainee(trainee_id: int):
-    return
 
+trainee_router = APIRouter(prefix="/trainees")
 
 # GET
-@trainee_router.get('/trainies/{trainee_id}')
-async def get_trainee(trainee_id: int):
-    return
+@trainee_router.get("/", response_model=List[TraineesResponse])
+async def get_trainees(
+    db: Session = Depends(get_db),
+    name: Optional[str] = Query(None),
+):
+    trainees = get_list_trainees(db=db, name=name)
+    return trainees
 
-@trainee_router.get('/profile/trainees')
-async def get_trainees():
-    return
+# POST
+@trainee_router.post("/", response_model=TraineeResponse)
+async def create(
+    trainee_data: CreateTrainee,
+    coach_id: int,
+    db: Session = Depends(get_db),
+):
+    try:
+        trainee = create_trainee(db=db, trainee_data=trainee_data, coach_id=coach_id)
+        return trainee
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Ошибка при создании тренирующегося")
